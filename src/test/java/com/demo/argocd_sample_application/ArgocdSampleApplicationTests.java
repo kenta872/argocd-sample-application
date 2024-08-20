5e0624ba383d82fafd8a681799d28781d77b8c5a
NakaiKenta <nk19940709nk@gmail.com>
2024-08-20 09:22:21 +0900

    first commit

---
+++ src/test/java/com/demo/argocd_sample_application/ArgocdSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.argocd_sample_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgocdSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
